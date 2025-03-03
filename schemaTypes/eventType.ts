a264a2c167a4ad159cf0e23d907c572ad2296f1d
Punyasloka Parida <punyasloka.parida@seba.swiss>
2025-03-03 16:28:55 +0530

    CMS

---
+++ schemaTypes/eventType.ts
@@ -0,0 +1,41 @@
+import {defineField, defineType} from 'sanity'
+
+export const eventType = defineType({
+    name: 'event',
+    title: 'Event',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'name',
+            type: 'string',
+            validation: (rule) => rule.required(),
+        }),
+        defineField({
+            name: 'slug',
+            type: 'slug',
+            options: {source: 'title'},
+            validation: (rule) => rule.required(),
+        }),
+        defineField({
+            name: 'startDate',
+            type: 'datetime',
+            initialValue: () => new Date().toISOString(),
+            validation: (rule) => rule.required(),
+        }),
+        defineField({
+            name: 'endDate',
+            type: 'datetime',
+            initialValue: () => new Date().toISOString(),
+            validation: (rule) => rule.required(),
+        }),
+        defineField({
+            name: 'image',
+            type: 'image',
+        }),
+        defineField({
+            name: 'brief',
+            type: 'array',
+            of: [{type: 'block'}],
+        }),
+    ],
+})
