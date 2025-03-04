8e56565ac39c4e3fd51a6053bd08ace88b501245
Punyaslok <12557052+bitsnorbytes@users.noreply.github.com>
2025-03-04 14:41:42 +0530

    CI / CD Workflow

--- schemaTypes/eventType.ts
+++ schemaTypes/eventType.ts
@@ -1,41 +1,45 @@
 import {defineField, defineType} from 'sanity'
 
 export const eventType = defineType({
-    name: 'event',
-    title: 'Event',
-    type: 'document',
-    fields: [
-        defineField({
-            name: 'name',
-            type: 'string',
-            validation: (rule) => rule.required(),
-        }),
-        defineField({
-            name: 'slug',
-            type: 'slug',
-            options: {source: 'title'},
-            validation: (rule) => rule.required(),
-        }),
-        defineField({
-            name: 'startDate',
-            type: 'datetime',
-            initialValue: () => new Date().toISOString(),
-            validation: (rule) => rule.required(),
-        }),
-        defineField({
-            name: 'endDate',
-            type: 'datetime',
-            initialValue: () => new Date().toISOString(),
-            validation: (rule) => rule.required(),
-        }),
-        defineField({
-            name: 'image',
-            type: 'image',
-        }),
-        defineField({
-            name: 'brief',
-            type: 'array',
-            of: [{type: 'block'}],
-        }),
-    ],
+  name: 'event',
+  title: 'Event',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'tagline',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'startDate',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'endDate',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+    }),
+    defineField({
+      name: 'brief',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+    defineField({
+      name: 'registrationLink',
+      type: 'url',
+      validation: (rule) => rule.required(),
+    }),
+  ],
 })
